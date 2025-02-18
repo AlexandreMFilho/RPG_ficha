a6189e2f1394098fbe3c8236f3ea8b8da02a2b98
filho.alexandre <filho.alexandre@graduacao.uerj.br>
2025-02-17 22:14:49 -0300

    Ficha ganhando corpo

--- rpg-primos/resources/views/fichas/index.blade.php
+++ rpg-primos/resources/views/fichas/index.blade.php
@@ -1,33 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container card" style="height:90%;width:100%;">
+<div class="container card" style="height:95%;">
     <h2 class="text-center text-uppercase" style="border:1px solid black">Ficha 1</h2>
 
-    <div style="display:flex;flex-direction:row">
+    <!-- <div  style="display:flex;flex-direction:row;"> -->
+    <div>
 
         <!-- Nome e imagem da Valquíria -->
-        <div class="row" style="border:1px solid black; height:400px;width:400px;margin-left:20px">
-            <div class="col-md-4 text-center">
-                <img src="" alt="Imagem da Valquíria" class="img-fluid rounded">
-            </div>
-            <div class="col-md-8">
-                <h3 class="text-danger"></h3>
+        <div class="col center" style="
+            display:flex;flex-direction:column;justify-content:center;align-items:center;
+            border:1px solid black; height:300px;width:300px;
+            ">
+            <div >
+                <img src="{{ asset('imgs/Max.jpg') }}" alt="Imagem da Valquíria" class="img-fluid">
             </div>
         </div>
     
         <!-- Atributos e Deslocamento -->
-        <div class="row mt-3" style="border:1px solid black">
-            <div class="col-md-6">
+        <div class="row" style="border:1px solid black">
+            <div class="col-md-6" style="display:flex;flex-direction:column;flex-wrap:wrap">
                 <h4>Atributos</h4>
                 <ul class="list-group">
-                        <li class="list-group-item"><strong>amuleto:</strong> 1 </li>
+                        <li class="list-group-item"><strong>Força:</strong> 1 </li>
+                        <li class="list-group-item"><strong>Destreza:</strong> 1 </li>
+                        <li class="list-group-item"><strong>Constituição:</strong> 1 </li>
+                        <li class="list-group-item"><strong>Inteligência:</strong> 1 </li>
+                        <li class="list-group-item"><strong>Percepção:</strong> 1 </li>
+                        <li class="list-group-item"><strong>Carisma:</strong> 1 </li>
+                        <li class="list-group-item"><strong>Agilidade:</strong> 1 </li>
+                        <li class="list-group-item"><strong>Vigor:</strong> 1 </li>
+                        <li class="list-group-item"><strong>Raciocínio:</strong> 1 </li>
                 </ul>
             </div>
-            <div class="col-md-6">
-                <h4>Deslocamento</h4>
-                <p class="lead">9 metros</p>
-            </div>
+        </div>
+        <div>
+            <h4>Deslocamento</h4>
+            <p class="lead">9 metros</p>
         </div>
     </div>
 
