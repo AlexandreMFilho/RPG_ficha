0330f9a874e79b91b65981dee1a517a94dddbb76
filho.alexandre <filho.alexandre@graduacao.uerj.br>
2025-03-31 17:11:58 -0300

    Compenente do personagem da Moldura criado

--- rpg-primos/resources/views/fichas/partials/CartaMana.blade.php
+++ rpg-primos/resources/views/fichas/partials/CartaMana.blade.php
@@ -1,23 +1,19 @@
-<div style="position: relative; display: inline-block; margin-bottom: 10px;
-    padding:1.5px">
+<div style="position: relative; display: inline-block; margin-bottom: 10px; padding: 1.5px">
 
     <img src="{{ asset('imgs/'.$imagem) }}" alt="Mana Planície" class="img-fluid" 
-        style="width: 40px; border: 0px solid #ccc;">
+        style="width: 40px; border: 0px solid #ccc;
+        ">
     
     <!-- Mana branca -->
     <span style="
         position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        display: flex;
-        align-items: flex-start; /* Alinha ao topo */
-        justify-content: center; /* Mantém centralizado horizontalmente */
-        padding-top: 3px; /* Ajuste fino para melhor posicionamento */
+        bottom: 0;
+        top: 10px;
+        left: 50%;
+        transform: translateX(-50%);
         font-size: 25px;
         color: white;
-        text-shadow: 0 0 0.2em #000, 0 0 0.2em #FFF;
+        text-shadow: 0 0 0.2em #000, 0 0 0.2em {{$color}};
         font-weight: bold;
     ">
         {{$valor}}
--- rpg-primos/resources/views/fichas/partials/MolduraPersonagem.blade.php
+++ rpg-primos/resources/views/fichas/partials/MolduraPersonagem.blade.php
@@ -1,50 +1,49 @@
-<div style="display:flex;flex-direction:row;
-            /* justify-content:space-around;align-items:flex-start; */
-            border:1px solid blue; height:300px;
-            margin:10px;padding:10px;
-            ">
+<div style="
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-end;
+    border: 1px solid blue;
+    height: 300px;
+    width: 300px;
+    margin: 10px;
+    padding: 10px;
+    position: relative;
+">
 
     <!-- Nome e imagem da Valquíria -->
     <div style="
-        display:flex;
-        position:absolute;
-        border:1px solid gray;
-        height:280px;max-width:300px;
+        display: flex;
+        position: relative;
+        border: 1px solid gray;
+        height: 280px;
+        width: 100%;
         border-radius: 8px;
-        ">
-            <img src="{{ asset('imgs/' . $imagem) }}"alt="Imagem da Valquíria" class="img-fluid" 
-            style="width:280px;
+    ">
+        <img src="{{ asset('imgs/' . $imagem) }}" alt="Imagem da Valquíria" class="img-fluid" 
+            style="width: 100%;
             border: 0px solid #ccc;
-            border-radius: 8px;
-            ">
-    </div>
-    <div>
-
-        @include('fichas.partials.CartaMana', ['imagem' =>'plains.jpg', 'valor' => '1'])
-        @include('fichas.partials.CartaMana', ['imagem' =>'plains.jpg', 'valor' => '1'])
-        @include('fichas.partials.CartaMana', ['imagem' =>'plains.jpg', 'valor' => '1'])
-        @include('fichas.partials.CartaMana', ['imagem' =>'plains.jpg', 'valor' => '1'])
-        @include('fichas.partials.CartaMana', ['imagem' =>'plains.jpg', 'valor' => '1'])
-        @include('fichas.partials.CartaMana', ['imagem' =>'plains.jpg', 'valor' => '1'])
+            border-radius: 8px;">
     </div>
 
-           
+    <!-- Ícones de mana alinhados ao fundo da imagem -->
+    <div style="
+        position: absolute;
+        width: 100%;
+        bottom: 5px;
+        left: 50%;
+        transform: translateX(-50%);
+        display: flex;
+        justify-content: center;
+        /* border: 2px solid green; */
+        gap: 5px; /* Espaçamento entre os ícones */
+    ">
+        @include('fichas.partials.CartaMana', ['imagem' =>'plains.jpg', 'valor' => '1', 'color' => '#FFF'])
+        @include('fichas.partials.CartaMana', ['imagem' =>'island.jpg', 'valor' => '1', 'color' => 'blue'])
+        @include('fichas.partials.CartaMana', ['imagem' =>'swamp.jpg', 'valor' => '1', 'color' => '#000'])
+        @include('fichas.partials.CartaMana', ['imagem' =>'montain.jpg', 'valor' => '1', 'color' => 'red'])
+        @include('fichas.partials.CartaMana', ['imagem' =>'forest.jpg', 'valor' => '1', 'color' => 'green'])
+        @include('fichas.partials.CartaMana', ['imagem' =>'colorless.jpg', 'valor' => '1', 'color' => '#fff'])
+    </div>
+
 </div>
-    
-                    <!-- Mana branca -->
-                    <span><strong style="font-size:25px; color:white; text-shadow: 0 0 0.2em #000, 0 0 0.2em #FFF">1</strong></span>
-
-                    <!-- Mana azul -->                    
-                    <span><strong style="font-size:25px; color:white; text-shadow: 0 0 0.2em #000, 0 0 0.2em blue">1</strong></span>
-
-                    <!-- Mana preta -->
-                    <span><strong style="font-size:25px; color:white; text-shadow: 0 0 0.2em #000, 0 0 0.2em #000">1</strong></span>
-
-                    <!-- Mana vermelha -->
-                    <span><strong style="font-size:25px; color:white; text-shadow: 0 0 0.2em #000, 0 0 0.2em red">1</strong></span>
-
-                    <!-- Mana verde -->
-                    <span><strong style="font-size:25px; color:white; text-shadow: 0 0 0.2em #000, 0 0 0.2em green">1</strong></span>
-
-                    <!-- Mana sem cor -->
-                    <span><strong style="font-size:25px; color:white; text-shadow: 0 0 0.2em #000, 0 0 0.2em #fff">1</strong></span>
