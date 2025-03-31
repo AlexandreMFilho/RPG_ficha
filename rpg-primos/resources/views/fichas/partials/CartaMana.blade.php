ee20ec003c5207f62dfe36264e891cba422419da
filho.alexandre <filho.alexandre@graduacao.uerj.br>
2025-03-31 13:57:46 -0300

    Continuar a mexer

---
+++ rpg-primos/resources/views/fichas/partials/CartaMana.blade.php
@@ -0,0 +1,19 @@
+<div style="
+    position:absolute;
+
+    height:100%;margin-bottom:10px;
+    ">
+
+
+
+
+    <img src="{{ asset('imgs/'.$imagem) }}" alt="Mana Planície" class="img-fluid" 
+        style="width:40px;
+        border: 0px solid #ccc;
+
+        ">
+    
+    <!-- Mana branca -->
+    <span><strong style="position:absolute; font-size:25px; color:white; text-shadow: 0 0 0.2em #000, 0 0 0.2em #FFF">{{$valor}}</strong></span>
+
+</div>
---
+++ rpg-primos/resources/views/fichas/partials/MolduraPersonagem.blade.php
@@ -0,0 +1,47 @@
+<div style="display:flex;flex-direction:row;
+            justify-content:space-around;align-items:flex-start;
+            border:1px solid blue; height:300px;
+            margin:10px;padding:10px;
+            ">
+
+    <!-- Nome e imagem da Valquíria -->
+    <div style="
+        display:flex;
+        justify-content:center;align-items:center;
+        border:1px solid gray;
+        height:280px;max-width:300px;
+        border-radius: 8px;
+        ">
+            <img src="{{ asset('imgs/' . $imagem) }}"alt="Imagem da Valquíria" class="img-fluid" 
+            style="width:280px;
+            border: 0px solid #ccc;
+            border-radius: 8px;
+            ">
+    </div>
+
+    @include('fichas.partials.CartaMana', ['imagem' =>'plains.jpg', 'valor' => '1'])
+    @include('fichas.partials.CartaMana', ['imagem' =>'plains.jpg', 'valor' => '1'])
+    @include('fichas.partials.CartaMana', ['imagem' =>'plains.jpg', 'valor' => '1'])
+    @include('fichas.partials.CartaMana', ['imagem' =>'plains.jpg', 'valor' => '1'])
+    @include('fichas.partials.CartaMana', ['imagem' =>'plains.jpg', 'valor' => '1'])
+    @include('fichas.partials.CartaMana', ['imagem' =>'plains.jpg', 'valor' => '1'])
+           
+</div>
+    
+                    <!-- Mana branca -->
+                    <span><strong style="font-size:25px; color:white; text-shadow: 0 0 0.2em #000, 0 0 0.2em #FFF">1</strong></span>
+
+                    <!-- Mana azul -->                    
+                    <span><strong style="font-size:25px; color:white; text-shadow: 0 0 0.2em #000, 0 0 0.2em blue">1</strong></span>
+
+                    <!-- Mana preta -->
+                    <span><strong style="font-size:25px; color:white; text-shadow: 0 0 0.2em #000, 0 0 0.2em #000">1</strong></span>
+
+                    <!-- Mana vermelha -->
+                    <span><strong style="font-size:25px; color:white; text-shadow: 0 0 0.2em #000, 0 0 0.2em red">1</strong></span>
+
+                    <!-- Mana verde -->
+                    <span><strong style="font-size:25px; color:white; text-shadow: 0 0 0.2em #000, 0 0 0.2em green">1</strong></span>
+
+                    <!-- Mana sem cor -->
+                    <span><strong style="font-size:25px; color:white; text-shadow: 0 0 0.2em #000, 0 0 0.2em #fff">1</strong></span>
